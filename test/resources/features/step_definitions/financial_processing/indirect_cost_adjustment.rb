56d6f95908857cd72999b1d4084b6b872c911153
douglas <douglashiura@gmail.com>
2019-05-22 18:55:03 -0300

    https://github.com/Vardot/varbase-behat.git

--- test/resources/features/step_definitions/financial_processing/indirect_cost_adjustment.rb
+++
@@ -1,6 +0,0 @@
-And /^I enter a Grant Accounting Line and a Receipt Accounting Line$/ do
-  on(IndirectCostAdjustmentPage) { @indirect_cost_adjustment.add_source_line({
-                                                                                account_number: '1278003',
-                                                                                amount: '2100'
-                                                                              }) }
-end
